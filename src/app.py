0cf1e8c0ca9196e2620e9484c9095a91cc62e093
Ignacio Pinto <51886210+Pinto2291@users.noreply.github.com>
2026-03-19 05:03:49 +0000

    endpoints creados, delete creado, postman usado para verificar requests

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+# Variable global todos para guardar las tareas en una list de dictionaries
+todos = [{"label": 'primera tarea', "done": False}, {"label": 'terminar el proyecto', "done": True}, {"label": 'usar postman', "done": True}]
 
 @app.route('/myroute', methods=['GET'])
 def hello_world():
@@ -8,12 +11,32 @@ def hello_world():
 
 @app.route('/todos', methods=['GET'])
 def todos_route():
-    return '<h1>Hello!</h1>'
+    json_text = jsonify(todos)
 
+    return json_text
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    #agregamos nuestra tarea al todos list
+    todos.append(request_body)
 
+    return jsonify(todos)
 
+#ENDPOINT y peticion para hacerle delete a los todos
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
 
+    # Verificacion de que esa posicion existe en la lista
+    if position >= len(todos):
+        print("Error: esa posicion no existe en la lista:", position)
+        return jsonify({"message": "Error: esa posicion no existe en la lista"}), 404
+    
+    todos.pop(position)
+
+    return jsonify(todos)
 
 
 # Estas 2 lineas siempre deben estar al final de la aplicacion
